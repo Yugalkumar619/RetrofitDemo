f32d5f459815f24f6301a06b7e1c5373ccaa2a69
Yugalkumar619 <71225190+Yugalkumar619@users.noreply.github.com>
2021-06-13 17:18:31 +0530

    Retrofit Instance class

---
+++ app/src/main/java/com/yugal/retofitdemo/RetrofitInstance.kt
@@ -0,0 +1,21 @@
+package com.yugal.retofitdemo
+
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitInstance {
+
+    companion object{
+        val BASE_URL = "https://jsonplaceholder.typicode.com/"
+        fun getRetofitInstance():Retrofit{
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+                .build()
+        }
+
+
+
+    }
+}
