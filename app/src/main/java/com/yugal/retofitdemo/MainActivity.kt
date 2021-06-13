b49d8fb11388003a63d337675d316bfbf49472cd
Yugalkumar619 <71225190+Yugalkumar619@users.noreply.github.com>
2021-06-13 18:06:05 +0530

    Showing data in the app

--- app/src/main/java/com/yugal/retofitdemo/MainActivity.kt
+++ app/src/main/java/com/yugal/retofitdemo/MainActivity.kt
@@ -2,10 +2,36 @@ package com.yugal.retofitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val retService = RetrofitInstance
+            .getRetofitInstance()
+            .create(AlbumService::class.java)
+
+        val responseLiveData:LiveData<Response<Album>> = liveData {
+            val response = retService.getAlbums()
+            emit(response)
+        }
+        responseLiveData.observe(this, Observer {
+            val albumsList = it.body()?.listIterator()
+            if(albumsList != null){
+                while(albumsList.hasNext()){
+                    val albumsItem = albumsList.next()
+                    val result : String = " "+ "Album Title: ${albumsItem.title}"+"\n"+
+                            " "+ "Album id: ${albumsItem.id}"+"\n"+
+                            " "+ "user id: ${albumsItem.userId}"+"\n\n\n"
+                    text_view.append(result)
+                }
+            }
+        })
     }
 }
